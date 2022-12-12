a3e139a6d86f194e6961565415840992b9e09499
Anand Dewangan <118416821+AnandDewangan@users.noreply.github.com>
2022-12-12 10:21:44 +0530

    Update Boolean_Matrix.java

--- Boolean_Matrix.java
+++ Boolean_Matrix.java
@@ -3,7 +3,7 @@
 
 import java.util.*;
 import java.io.*;
-class Main {
+class Boolean_Matrix {
     public static void main (String args[])throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int t = Integer.parseInt(br.readLine());
